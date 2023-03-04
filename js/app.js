11d08990d94f76605219f0d6a34a3834a6df1efb
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2023-03-04 11:56:02 -0300

    Practica1

---
+++ js/app.js
@@ -0,0 +1,4 @@
+alert("Ingresa tu nombre");
+var nombre = prompt("Ingresa tu nombre");
+document.write("Bienvenido: " + nombre + " saludos desde JavaScrpit");
+console.log("Mensaje privado de: " + nombre);
